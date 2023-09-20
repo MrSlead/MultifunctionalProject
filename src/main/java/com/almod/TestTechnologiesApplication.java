17e579dc15b8ad1b9888bb894022f079c3087805
Alex <mordyasov.60@mail.ru>
2023-09-20 19:05:58 +0700

    Initialization of the project

---
+++ src/main/java/com/almod/TestTechnologiesApplication.java
@@ -0,0 +1,16 @@
+package com.almod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechnologiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTechnologiesApplication.class, args);
+
+        System.out.println("Hello world!");
+
+    }
+
+}
---
+++ src/test/java/com/almod/TestTechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechnologiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
