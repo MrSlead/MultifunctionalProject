b287dc9ba03b151a98544fccbd5b20a575ad92e5
Alex <mordyasov.60@mail.ru>
2023-09-22 16:56:01 +0700

    Added lombok and jackson format for date

---
+++ src/main/java/com/almod/flow/broker/activemq/config/JacksonConfiguration.java
@@ -0,0 +1,26 @@
+package com.almod.flow.broker.activemq.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return jacksonObjectMapperBuilder -> {
+            // formatter
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            // deserializers
+            jacksonObjectMapperBuilder.deserializers(new LocalDateDeserializer(dateFormatter));
+
+            // serializers
+            jacksonObjectMapperBuilder.serializers(new LocalDateSerializer(dateFormatter));
+        };
+    }
+}
--- src/main/java/com/almod/flow/broker/activemq/entity/ActiveMQClientRequest.java
+++ src/main/java/com/almod/flow/broker/activemq/entity/ActiveMQClientRequest.java
@@ -1,57 +1,28 @@
 package com.almod.flow.broker.activemq.entity;
 
 import com.almod.flow.broker.entity.AbstractClientRequest;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.time.LocalDate;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(includeFieldNames = true)
 public class ActiveMQClientRequest extends AbstractClientRequest {
     private String firstName;
+
     private String lastName;
+
     private String patronymic;
-    private String dateOfBirth;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dateOfBirth;
+
     private String serialNumber;
+
     private String placeOfResidence;
-
-    public ActiveMQClientRequest(String firstName, String lastName, String patronymic, String dateOfBirth, String serialNumber, String placeOfResidence) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.patronymic = patronymic;
-        this.dateOfBirth = dateOfBirth;
-        this.serialNumber = serialNumber;
-        this.placeOfResidence = placeOfResidence;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public String getPlaceOfResidence() {
-        return placeOfResidence;
-    }
-
-    @Override
-    public String toString() {
-        return "ClientRequest{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", dateOfBirth='" + dateOfBirth + '\'' +
-                ", serialNumber='" + serialNumber + '\'' +
-                ", placeOfResidence='" + placeOfResidence + '\'' +
-                '}';
-    }
 }
