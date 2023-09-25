e2073a9ea23a67b15d5e9166d1e6ecd8d3fab542
Alex <mordyasov.60@mail.ru>
2023-09-25 22:17:36 +0700

    Added exception handling in the ActiveMQPersonalCardToDBRoute

--- src/main/java/com/almod/flow/broker/activemq/route/ActiveMQPersonalCardToDBRoute.java
+++ src/main/java/com/almod/flow/broker/activemq/route/ActiveMQPersonalCardToDBRoute.java
@@ -3,24 +3,24 @@ package com.almod.flow.broker.activemq.route;
 import com.almod.flow.broker.activemq.repo.ActiveMQPersonalCardRepo;
 import com.almod.flow.broker.activemq.service.ActiveMQPersonalCardService;
 import com.almod.flow.broker.activemq.util.ActiveMQPersonalCardHandlerQueue;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActiveMQPersonalCardToDBRoute extends RouteBuilder {
-    ActiveMQPersonalCardRepo requestClientRepo;
-
     @Override
     public void configure() throws Exception {
-        /*from("timer:active-mq-timer?period=6000")
-                .routeId("Timer")
-                .log("Moving the message to the incoming.files")
-                .to("activemq:incoming.files");;*/
+        onException(Exception.class)
+                .handled(true)
+                .log(LoggingLevel.ERROR, "Error when sending to the db: \n${exception.cause}")
+                .setBody(simple("${exception.stacktrace}"))
+                .to("activemq:error");
 
          from("activemq:incoming.files")
                 .routeId("activemq-to-mysql")
-                .log("Try send a message to the db!")
-                //.bean(ActiveMQClientRequestHandlerQueue.class, "save(${header.clientRequestString})");
-                .bean(ActiveMQPersonalCardHandlerQueue.class, "save");
+                .log("Try send a message to the db")
+                .bean(ActiveMQPersonalCardService.class, "save");
     }
 }
+
