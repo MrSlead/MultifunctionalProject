1b2b8c54ac9593994565322ab63969ed23a0393e
Alex <mordyasov.60@mail.ru>
2023-09-27 03:36:18 +0700

    Removed redundant dependency

--- src/main/java/com/almod/flow/broker/type/activemq/service/ActiveMQPersonalCardService.java
+++ src/main/java/com/almod/flow/broker/type/activemq/service/ActiveMQPersonalCardService.java
@@ -1,7 +1,4 @@
 package com.almod.flow.broker.type.activemq.service;
 
-import com.almod.common.service.AbstractService;
-import com.almod.flow.broker.type.activemq.entity.ActiveMQPersonalCard;
-
-public interface ActiveMQPersonalCardService extends AbstractService<ActiveMQPersonalCard, Long> {
+public interface ActiveMQPersonalCardService {
 }
--- src/main/java/com/almod/flow/cache/hazelcast/service/HazelcastProductService.java
+++ src/main/java/com/almod/flow/cache/hazelcast/service/HazelcastProductService.java
@@ -1,7 +1,4 @@
 package com.almod.flow.cache.hazelcast.service;
 
-import com.almod.common.service.AbstractService;
-import com.almod.flow.cache.hazelcast.entity.HazelcastProduct;
-
-public interface HazelcastProductService extends AbstractService<HazelcastProduct, Long> {
+public interface HazelcastProductService {
 }
