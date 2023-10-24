e735093e2d5720871342a44680e637ba54c67153
Alex <mordyasov.60@mail.ru>
2023-10-24 22:31:13 +0700

    Fixed readme

--- src/main/java/com/almod/flow/broker/common/util/TransferDataToActiveMQ.java
+++ src/main/java/com/almod/flow/broker/common/util/TransferDataToActiveMQ.java
@@ -1,8 +1,7 @@
 package com.almod.flow.broker.common.util;
 
-import com.almod.flow.broker.type.activemq.ConstantsFlowBroker;
-import com.almod.flow.broker.type.activemq.ObjectMapperSingleton;
 import com.almod.common.entity.AbstractEntity;
+import com.almod.flow.broker.type.activemq.ObjectMapperSingleton;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.jms.TextMessage;
 import org.slf4j.Logger;
@@ -21,7 +20,7 @@ public class TransferDataToActiveMQ implements AbstractTransferDataToBroker<Abst
     }
 
     public void transferData(AbstractEntity clientRequest, String queue) throws JsonProcessingException {
-        LOGGER.info(String.format("[%s] Try to send a data to the queue %s", clientRequest.getUUID(), ConstantsFlowBroker.ACTIVEMQ_FLOW_PERSONAL_CARD_QUEUE));
+        LOGGER.info(String.format("[%s] Try to send a data to the queue %s", clientRequest.getUUID(), queue));
         String clientRequestString = ObjectMapperSingleton.getCustomizedObjectMapper().writeValueAsString(clientRequest);
 
         jmsTemplate.send(queue, session -> {
