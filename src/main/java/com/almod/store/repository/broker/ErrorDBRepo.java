a821c96be5187477e2b39d5d8d0cb42fbe0dfd18
mordy <mordyasov.60@mail.ru>
2024-07-10 15:55:56 +0700

    Изменение имен для entity сущностей

--- src/main/java/com/almod/store/repository/broker/ErrorDBRepo.java
+++ src/main/java/com/almod/store/repository/broker/ErrorDBRepo.java
@@ -1,9 +1,9 @@
 package com.almod.store.repository.broker;
 
-import com.almod.store.entity.ErrorDB;
+import com.almod.store.entity.ErrorDBEntity;
 import com.almod.store.repository.AbstractRepo;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ErrorDBRepo extends AbstractRepo<ErrorDB, Long> {
+public interface ErrorDBRepo extends AbstractRepo<ErrorDBEntity, Long> {
 }
--- src/main/java/com/almod/store/service/broker/ErrorDBServiceImpl.java
+++ src/main/java/com/almod/store/service/broker/ErrorDBServiceImpl.java
@@ -1,12 +1,12 @@
 package com.almod.store.service.broker;
 
 import com.almod.store.service.AbstractServiceImpl;
-import com.almod.store.entity.ErrorDB;
+import com.almod.store.entity.ErrorDBEntity;
 import com.almod.store.repository.broker.ErrorDBRepo;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ErrorDBServiceImpl extends AbstractServiceImpl<ErrorDB, Long> implements ErrorDBService {
+public class ErrorDBServiceImpl extends AbstractServiceImpl<ErrorDBEntity, Long> implements ErrorDBService {
     public ErrorDBServiceImpl(ErrorDBRepo repo) {
         super(repo);
     }
